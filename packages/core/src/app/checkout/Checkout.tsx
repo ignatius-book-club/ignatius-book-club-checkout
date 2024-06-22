dbec8339405b7015f79ad67d8cedfae6c0bf8126
Kacy Sommers <kacysommers@gmail.com>
2024-06-22 10:22:06 -0400

    adding brother-1816 font

--- packages/core/src/app/checkout/Checkout.tsx
+++ packages/core/src/app/checkout/Checkout.tsx
@@ -284,33 +284,44 @@ class Checkout extends Component<
 
     return (
       <div
-        className={classNames({
+        className={classNames('font-brother-1816', {
           'is-embedded': isEmbedded(),
           'remove-checkout-step-numbers': isHidingStepNumbers,
         })}
         data-test="checkout-page-container"
         id="checkout-page-container"
       >
-        <div className="header-top-container ibc-container" style={{ height: '70px' }}>
-          <a href={`${ibcUrl}/cart`} style={{ filter: 'brightness(0) invert(1)' }}>
-            <img
-              src="https://res.cloudinary.com/dsewycgig/image/upload/v1705889390/ibf_assets/ibf-brightblue_1701195858__78176.original_u75ege.png"
-              alt="Ignatius Book Fairs"
-              className="rounded-none"
-              width="180px"
-              height="76px"
-            />
-          </a>
-          <div className="header-top-links-container">
-            <a href="/"> Donate </a>
-            <a href="/"> Book Fairs </a>
-            <a href="/"> Book Club </a>
-            <a
-              href="/"
-              className="text-ibc-orange rounded-md bg-transparent bg-white px-4 py-2 uppercase"
-            >
-              Shop
+        <div className="bg-ibc-orange ibc-container" style={{ height: '70px' }}>
+          <div className="ibc-container-fixed flex h-full items-center justify-between">
+            <a href={`${ibcUrl}/cart`} style={{ filter: 'brightness(0) invert(1)' }}>
+              <img
+                src="https://res.cloudinary.com/dsewycgig/image/upload/v1705889390/ibf_assets/ibf-brightblue_1701195858__78176.original_u75ege.png"
+                alt="Ignatius Book Fairs"
+                className="rounded-none"
+                width="180px"
+                height="76px"
+              />
             </a>
+            <div className="hidden items-center gap-2 md:flex font-brother-1816">
+              <a
+                className="rounded-md bg-transparent px-4 py-2 uppercase text-white hover:opacity-70"
+                href={ibcUrl}
+              >
+                Book Fairs
+              </a>
+              <a
+                href={ibcUrl}
+                className="text-ibc-orange rounded-md bg-transparent bg-white px-4 py-2 uppercase"
+              >
+                Shop
+              </a>
+              <a
+                className="rounded-md bg-transparent px-4 py-2 uppercase text-white hover:opacity-70"
+                href="https://afvapnqh.donorsupport.co/-/XAKURTAP"
+              >
+                Donate
+              </a>
+            </div>
           </div>
         </div>
 
--- packages/core/src/app/checkout/utils/checkout-utils.ts
+++ packages/core/src/app/checkout/utils/checkout-utils.ts
@@ -1,4 +1,4 @@
 export const ibcUrl =
   process.env.NODE_ENV === 'development'
-    ? 'https://localhost:3002/checkout?edit=billing'
-    : 'https://www.ignatiusbookfairs.com/checkout?edit=billing';
+    ? 'https://localhost:3002'
+    : 'https://www.ignatiusbookfairs.com';
